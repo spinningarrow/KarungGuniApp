29d3a1476a78efba6b109c1bc7310ce915331ec6
qingemeng <qingemeng@gmail.com>
2013-10-02 15:05:11 +0800

    checking in "getting location with GPS"

---
+++ src/com/onemore/karungguniapp/NearbyAds.java
@@ -0,0 +1,11 @@
+package com.onemore.karungguniapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gemengqin
+ * Date: 10/2/13
+ * Time: 2:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NearbyAds {
+}
