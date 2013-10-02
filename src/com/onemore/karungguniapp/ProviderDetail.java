2a06227af3932cb18ca9ac58b59c50ff10ee8411
qingemeng <qingemeng@gmail.com>
2013-10-02 16:44:20 +0800

    retrieving the GPS info

--- src/com/onemore/karungguniapp/ProviderDetail.java
+++ src/com/onemore/karungguniapp/ProviderDetail.java
@@ -4,8 +4,8 @@ package com.onemore.karungguniapp;
  * Created with IntelliJ IDEA.
  * User: gemengqin
  * Date: 10/2/13
- * Time: 2:31 PM
+ * Time: 4:00 PM
  * To change this template use File | Settings | File Templates.
  */
-public class NearbyAds {
+public class ProviderDetail {
 }
