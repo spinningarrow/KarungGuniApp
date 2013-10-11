59f5d7667abffe163795584908ab7968c254a433
syphonator <chris1@e.ntu.edu.sg>
2013-10-11 13:18:40 +0800

    Fixed SHA1

--- src/com/onemore/karungguniapp/SHA1.java
+++ src/com/onemore/karungguniapp/SHA1.java
@@ -1,32 +1,32 @@
 package com.onemore.karungguniapp;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import android.util.Base64;
 import android.util.Log;
 
 public class SHA1 {
 
 	// SHA1 variables
 	private static String SHAHash;
-	private static int NO_OPTIONS=0;
+	private static final String HEX_DIGITS = "0123456789abcdef";
+	private static final int BYTE_MSK = 0xFF;
+	private static final int HEX_DIGIT_MASK = 0xF;
+    private static final int HEX_DIGIT_BITS = 4;
 	
 	/**
 	* Used by computeSHAHash
 	*/	
-	private static String convertToHex(byte[] data) throws java.io.IOException 
-	{         
-		StringBuffer sb = new StringBuffer();
-		String hex=null;
-		 
-		hex=Base64.encodeToString(data, 0, data.length, NO_OPTIONS);
-		 
-		sb.append(hex);
-		             
-		return sb.toString();
+	private static String convertToHex(byte[] data)
+	{   
+		StringBuilder sb = new StringBuilder(data.length * 2);
+        for (int i = 0; i < data.length; i++) {
+                int b = data[i] & BYTE_MSK;
+                sb.append(HEX_DIGITS.charAt(b >>> HEX_DIGIT_BITS)).append(
+                                HEX_DIGITS.charAt(b & HEX_DIGIT_MASK));
+        }
+        return sb.toString();
 	}
 	 
 	/**
@@ -36,25 +36,20 @@ public class SHA1 {
 	{
 		MessageDigest mdSha1 = null;
 		try {
-		  mdSha1 = MessageDigest.getInstance("SHA-1");
-	}
-	catch (NoSuchAlgorithmException e1) {
-		Log.e("myapp", "Error initializing SHA1 message digest");
-	}
-	try {
-		mdSha1.update(password.getBytes("ASCII"));
+			mdSha1 = MessageDigest.getInstance("SHA-1");
+		}
+		catch (NoSuchAlgorithmException e1) {
+			Log.e("myapp", "Error initializing SHA1 message digest");
+		}
+		try {
+			mdSha1.update(password.getBytes("ASCII"));
+			byte[] data = mdSha1.digest();
+			SHAHash=convertToHex(data);
 		}
 		catch (UnsupportedEncodingException e) {
 		    e.printStackTrace();
 		}
-		byte[] data = mdSha1.digest();
-		try {
-			SHAHash=convertToHex(data);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+
 		return SHAHash;
 	}
-
 }
