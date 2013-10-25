bb947258b5e2a10b4111d9c0e679cae4756300bd
syphonator <chris1@e.ntu.edu.sg>
2013-10-25 15:55:07 +0800

    Logout function and action bar added to Seller & KG pages

--- src/com/onemore/karungguniapp/KarungGuniActivity.java
+++ src/com/onemore/karungguniapp/KarungGuniActivity.java
@@ -1,9 +1,10 @@
 package com.onemore.karungguniapp;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.preference.PreferenceManager;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class KarungGuniActivity extends Activity {
@@ -16,14 +17,33 @@ public class KarungGuniActivity extends Activity {
         TextView tv = new TextView(this);
         tv.setTextSize(40);
         tv.setText(AccountManager.getCurrentUser(this).getString("email"));
+        getActionBar();
         setContentView(tv);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.karung_guni, menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.edit_profile:
+	            //TODO
+	            return true;
+	        case R.id.logout:
+	            AccountManager.clearCurrentUser(getApplicationContext());
+	            Intent i = new Intent(getBaseContext(), Main.class);
+	            startActivity(i);
+	            finish();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 }
--- src/com/onemore/karungguniapp/SellerActivity.java
+++ src/com/onemore/karungguniapp/SellerActivity.java
@@ -2,7 +2,9 @@ package com.onemore.karungguniapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class SellerActivity extends Activity {
 
@@ -15,8 +17,26 @@ public class SellerActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.seller, menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.edit_profile:
+	            //TODO
+	            return true;
+	        case R.id.logout:
+	            AccountManager.clearCurrentUser(getApplicationContext());
+	            Intent i = new Intent(getBaseContext(), Main.class);
+	            startActivity(i);
+	            finish();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 }
