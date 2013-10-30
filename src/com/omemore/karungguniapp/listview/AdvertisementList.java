1e817658cf607e643e52a1db19b20ab0a3f1681b
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2013-10-30 23:27:48 +0800

    Missed a file in previous commit (seller fix)

--- src/com/omemore/karungguniapp/listview/AdvertisementList.java
+++ src/com/omemore/karungguniapp/listview/AdvertisementList.java
@@ -1,12 +1,13 @@
 package com.omemore.karungguniapp.listview;
 
+import java.util.List;
+
 import android.app.ListFragment;
 import android.app.LoaderManager;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,16 +18,11 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
+
 import com.onemore.karungguniapp.AppData;
 import com.onemore.karungguniapp.KGApp;
 import com.onemore.karungguniapp.R;
 import com.onemore.karungguniapp.model.Advertisement;
-
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AdvertisementList extends ListFragment
 implements LoaderManager.LoaderCallbacks<Cursor> 
@@ -34,7 +30,7 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 
 	// This is the Adapter being used to display the list's data.
 	SimpleCursorAdapter mAdapter;
-		private KGApp app;
+	private KGApp app;
 	//	MongoAdapter mAdapter;
 
 
@@ -46,47 +42,26 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 	Context mContext;
 	List<Advertisement> ads;
 	ImageLoader imageLoader;
+	String selection = null;
+	String sortOrder = null;
+	String specialColumn;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) 
 	{
 		super.onActivityCreated(savedInstanceState);
-
-		// Give some text to display if there is no data. 
+		Bundle args = getArguments();
+		selection = args.getString("selection");
+		sortOrder = args.getString("orderby");
+		specialColumn = args.getString("column");
 		setEmptyText(getResources().getString(R.string.noData));
-				app = (KGApp)getActivity().getApplication();
-		//		mAdapter = new MongoAdapter(getActivity(), R.layout.advertisement);
-		// We have a menu item to show in action bar.
+		app = (KGApp)getActivity().getApplication();
 		setHasOptionsMenu(true);
 		mListView = getListView();
-
-		LayoutInflater inflator = getActivity().getLayoutInflater();
-		View header = inflator.inflate(R.layout.list_header, null);
-		ads = new ArrayList<Advertisement>();
-		//		header.setOnClickListener(new OnClickListener()
-		//		{
-		//			   @Override
-		//			   public void onClick(View v)
-		//			   {
-		//				   mAdapter.updateAdvertisements();
-		//			   }
-		//			});
-		//		
-		//		mListView.addHeaderView(header);
-		//		View footer = inflator.inflate(R.layout.list_footer, null);
-		//		footer.setOnClickListener(new OnClickListener()
-		//		{
-		//			   @Override
-		//			   public void onClick(View v)
-		//			   {
-		//				   mAdapter.loadMore();
-		//			   }
-		//			});
-		//		mListView.addFooterView(footer);
-        mContext = getActivity();
+		mContext = getActivity();
 		mAdapter = new SimpleCursorAdapter(getActivity(),
 				R.layout.advertisement, null,
-				new String[] { AppData.Advertisements.COLUMN_NAME_TITLE, AppData.Advertisements.COLUMN_NAME_DESCRIPTION, AppData.Advertisements.COLUMN_NAME_PHOTO},
+				new String[] { AppData.Advertisements.COLUMN_NAME_TITLE, specialColumn, AppData.Advertisements.COLUMN_NAME_PHOTO},
 				new int[] { R.id.title, R.id.distance, R.id.list_image }, 0);
 		imageLoader=new ImageLoader(getActivity().getApplicationContext());
 		mAdapter.setViewBinder(new ViewBinder() {
@@ -95,15 +70,9 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
 				if (view.getId() == R.id.list_image) {
 					ImageView image = (ImageView) view;
-					String url;
-					try {
-						url = new String(cursor.getBlob(columnIndex), "UTF-8");
-						imageLoader.DisplayImage(url, image);
-						return true;
-					} catch (UnsupportedEncodingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					String url = cursor.getString(columnIndex);
+					imageLoader.DisplayImage(url, image);
+					return true;
 				}
 				return false;
 			}
@@ -121,16 +90,6 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 		getLoaderManager().initLoader(0, null, this);
 	}
 
-	public static Drawable LoadImageFromWebOperations(String url) {
-		try {
-			InputStream is = (InputStream) new URL(url).getContent();
-			Drawable d = Drawable.createFromStream(is, "src name");
-			return d;
-		} catch (Exception e) {
-			return null;
-		}
-	}
-
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = super.onCreateView(inflater, container, savedInstanceState);
@@ -138,26 +97,7 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 		parent.addView(v, 0);
 		return parent;
 	}
-	
-	//	public boolean onQueryTextChange(String newText) {
-	//		// Called when the action bar search text has changed.  Update
-	//		// the search filter, and restart the loader to do a new query
-	//		// with this filter.
-	//		String newFilter = !TextUtils.isEmpty(newText) ? newText : null;
-	//		// Don't do anything if the filter hasn't actually changed.
-	//		// Prevents restarting the loader when restoring state.
-	//		if (mCurFilter == null && newFilter == null) {
-	//			return true;
-	//		}
-	//		if (mCurFilter != null && mCurFilter.equals(newFilter)) {
-	//			return true;
-	//		}
-	//		mCurFilter = newFilter;
-	//		getLoaderManager().restartLoader(0, null, this);
-	//		return true;
-	//	}
-	////
-	
+
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 		// This is called when a new Loader needs to be created.  This
 		// sample only has one Loader, so we don't care about the ID.
@@ -167,8 +107,7 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 		// Now create and return a CursorLoader that will take care of
 		// creating a Cursor for the data being displayed.
 		return new CursorLoader(getActivity(), baseUri,
-				null, null, null,
-				null);
+				null, selection, null, sortOrder);
 	}
 
 	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
